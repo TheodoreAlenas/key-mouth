704935a1d896c02dd2a1edf4ed70d6f9afc72d9a
Theodore Alenas <dimakopt732@gmail.com>
2024-07-27 09:14:27 +0300

    *-* -> *_* & tried fastapi html, index unresolved

--- web_back/main.py
+++ web_back/main.py
@@ -1,8 +1,13 @@
 from a_feature import say_hi
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/message")
 async def root():
     return {"message": say_hi()}
+
+app.mount("/pages",
+          StaticFiles(directory="../web_front/.next/server/pages"),
+          name="pages")
