89e145016dd8b27003dce56e8e57102455b65195
Theodore Alenas <dimakopt732@gmail.com>
2024-07-29 15:40:15 +0300

    dummy table and ul messages

--- web_front/pages/index.js
+++ web_front/pages/index.js
@@ -1,5 +1,25 @@
 import sayHi from '../../a_feature/say_hi.mjs'
 
 export default function Home() {
-    return <h1>{sayHi()}</h1>
+    return (
+        <>
+            <h1>{sayHi()}</h1>
+            <table>
+                <thead>
+                    <tr><td>Name</td><td>Message</td></tr>
+                </thead>
+                <tbody>
+                    <tr><td>Sotiris</td><td>Hi Mark</td></tr>
+                    <tr><td>Sotiris</td><td>Are you there?</td></tr>
+                </tbody>
+            </table>
+            <section>
+                <ul>
+                    <li>Hi Mark</li>
+                    <li>Are you there?</li>
+                </ul>
+            </section>
+            <input type="text" />
+            <button>Send and clear</button>
+        </>)
 }
