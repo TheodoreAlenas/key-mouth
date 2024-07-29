5f8551d2c7015156bbebf9aa5b45182788d996ba
Theodore Alenas <dimakopt732@gmail.com>
2024-07-29 16:11:50 +0300

    got lost in my thoughts stirring index.js

--- web_front/pages/index.jsx
+++ web_front/pages/index.jsx
@@ -2,7 +2,7 @@ import sayHi from '../../a_feature/say_hi.mjs'
 import { useState } from "react"
 
 export default function Home() {
-    let [s, ss] = useState(
+    let [messages, setMessages] = useState(
         [{name: "Sotiris", msg: "Hi Mark"},
          {name: "Sotiris", msg: "Are you there?"}])
     return (
@@ -10,16 +10,12 @@ export default function Home() {
             <h1>{sayHi()}</h1>
             <table>
                 <tbody>
-                  {s.map(e => <tr><td>{e.name}</td><td>{e.msg}</td></tr>)}
+                    { messages.map(e =>
+                        <tr><td>{e.name}</td><td>{e.msg}</td></tr>) }
                 </tbody>
             </table>
-            <section>
-                <ul>
-                    <li>Hi Mark</li>
-                    <li>Are you there?</li>
-                </ul>
-            </section>
             <input type="text" />
-            <button onClick={function() {ss([])}}>Send and clear</button>
+            <button onClick={ function() {setMessages([])} }
+            >Send and clear</button>
         </>)
 }
