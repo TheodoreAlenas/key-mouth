32c3bf8906546386ed5b971938da6d44842bd23f
Theodore Alenas <dimakopt732@gmail.com>
2024-07-29 17:52:21 +0300

    web socket goes both ways

--- web_back/main.py
+++ web_back/main.py
@@ -1,9 +1,15 @@
 from a_feature import say_hi
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/message")
-async def root():
-    return {"message": say_hi()}
+@app.websocket("/")
+async def root(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message was {data}")
+    except WebSocketDisconnect as e:
+        pass
--- web_front/pages/index.jsx
+++ web_front/pages/index.jsx
@@ -1,5 +1,4 @@
 import sayHi from '../../a_feature/say_hi.mjs'
-import Socket from '../sockets.mjs'
 import { useEffect, useState } from "react"
 
 export default function Home() {
@@ -9,14 +8,15 @@ export default function Home() {
     const messageInputID = "message-input"
     useEffect(function() {
         console.log("start")
-        const s = new Socket(
-            function() {
-                return document.getElementById(messageInputID).value
-            },
-            function(newOutput) {
-                setMessages(newOutput)
-            }
-        )
+        const s = new WebSocket("ws://localhost:8000")
+        const d = "date is " + Date.now()
+        s.addEventListener("open", function(event) {
+            s.send(d)
+        })
+        s.addEventListener("message", function(event) {
+            if (event.data === "Message was " + d) console.log("same")
+            else console.log("different")
+        })
         return function() {
             console.log("end")
             s.close()
