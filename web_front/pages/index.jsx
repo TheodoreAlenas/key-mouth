3a5fceb3cfe9fc1aaf5510027094c1130ff99ea1
Theodore Alenas <dimakopt732@gmail.com>
2024-07-30 10:55:44 +0300

    front: used a form with a submit button

--- web_front/pages/index.jsx
+++ web_front/pages/index.jsx
@@ -1,27 +1,37 @@
 import sayHi from '../../a_feature/say_hi.mjs'
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function Home() {
+    const [inputValue, setInputValue] = useState('')
+    const preventDefClearInp = function(event) {
+        event.preventDefault()
+        setInputValue("")
+        socketRef.current.send("cleared")
+    }
+    const f = function(event) {
+        const v = event.target.value
+        setInputValue(v)
+        socketRef.current.send(v)
+    }
     let [messages, setMessages] = useState(
         [{name: "Sotiris", message: "Hi Mark"},
          {name: "Sotiris", message: "Are you there?"}])
-    const messageInputID = "message-input"
+    const inputId = "message-input"
+    const socketRef = useRef(null)
     useEffect(function() {
         console.log("start")
-        const s = new WebSocket("ws://localhost:8000")
+        socketRef.current = new WebSocket("ws://localhost:8000")
         const d = "date is " + Date.now()
-        s.addEventListener("open", function(event) {
-            s.send(d)
+        socketRef.current.addEventListener("open", function(event) {
+            socketRef.current.send(d)
         })
-        s.addEventListener("message", function(event) {
+        socketRef.current.addEventListener("message", function(event) {
             if (event.data === "Message was " + d) console.log("same")
-            else console.log("different")
+            else console.log("different: " + event.data)
         })
-
-        prevInputBoxValue = document.getElementById(messageInputID).value
         return function() {
             console.log("end")
-            s.close()
+            socketRef.current.close()
         }
     }, [])
     return (
@@ -39,24 +49,13 @@ export default function Home() {
                     }
                 </tbody>
             </table>
-            <input id={ messageInputID } type="text" onChange={ function() {
-                       const v = document.getElementById(messageInputID).value
-                       console.log(diff(prevInputBoxValue, v))
-                       prevInputBoxValue = v
-                   } } />
-            <button onClick={ function() { setMessages([]) } }
-            >Send and clear</button>
+            <form onSubmit={preventDefClearInp}>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={f}
+                />
+                <button type="submit">Clear</button>
+            </form>
         </>)
-}
-
-let prevInputBoxValue = ""
-
-function diff(a, b) {
-    if (b.startsWith(a)) {
-        return b.substr(a.length)
-    }
-    if (a.startsWith(b)) {
-        return a.length - b.length
-    }
-    return { a, b }
 }
