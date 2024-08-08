a1d2cefffd448db5252104833cbc23bc46b5f5f9
Theodore Alenas <dimakopt732@gmail.com>
2024-08-08 17:59:42 +0300

    systests all the way! They're good!

--- systest-check.py
+++ systest-check.py
@@ -8,23 +8,19 @@ class BackCheck(unittest.TestCase):
         with open("git-ignores/systest-logs-back") as f:
             self.lines = f.readlines()
 
-    def one_says(self, text):
-        for i in range(self.cur, len(self.lines)):
-            if self.lines[i].find(text):
-                return True
-        return False
-
     def setUp(self):
         self.cur = 0
 
-    def test_requested_socket_and_last_messages(self):
-        self.assertTrue(self.one_says(' - "GET /last HTTP/1.1" 200'))
-        self.cur = 0
-        self.assertTrue(self.one_says(' - "WebSocket /" [accepted]'))
-
-    def test_connection_open_and_closed(self):
-        self.assertTrue(self.one_says(' - connection open\n'))
-        self.assertTrue(self.one_says(' - connection closed\n'))
+    def test_open_connections_closed(self):
+        openConnections = 0
+        closedConnections = 0
+        for l in self.lines:
+            if l.find(' - connection open\n') != -1:
+                openConnections += 1
+            elif l.find(' - connection closed\n') != -1:
+                closedConnections += 1
+        self.assertEqual(4, openConnections)
+        self.assertEqual(openConnections, closedConnections)
 
     def test_gracefuly_shut_down(self):
         self.assertTrue(self.lines[-3].find(" - Waiting for application shutdown.\n") != -1)
@@ -39,32 +35,96 @@ class FrontCheck(unittest.TestCase):
         with open("git-ignores/systest-logs-front") as f:
             self.lines = f.readlines()
 
-    def one_says(self, text):
-        for i in range(self.cur, len(self.lines)):
-            if self.lines[i].find(text):
-                return True
-        return False
+    def test_input_value_is_set_at_least_once(self):
+        a = False
+        for l in self.lines:
+            if l.find("\tinput value set to '") != -1:
+                a = True
+        self.assertTrue(a)
 
-    def setUp(self):
-        self.cur = 0
+    def test_broadcaster_heard_the_same_as_listener(self):
+        bcaster_heard = []
+        listener_heard = []
+        say_last = "\t[say]\tlast moment set to ["
+        hear_last = "\t[hear]\tlast moment set to ["
+        for l in self.lines:
+            pos = l.find(say_last)
+            if pos != -1:
+                bcaster_heard.append(l[pos + len(say_last):])                
+        for l in self.lines:
+            pos = l.find(hear_last)
+            if pos != -1:
+                listener_heard.append(l[pos + len(hear_last):])
+        self.assertEqual(bcaster_heard, listener_heard)
+        self.assertEqual(4, len(bcaster_heard))
 
-    def test_old_moments_got_set(self):
-        self.assertTrue(self.one_says("\told moments set to ["))
+    def test_after_interrupt_old_moments_filled(self):
+        stop_last = "<stop last wasn't set>"
+        stop_old = "<stop old wasn't set>"
+        all_old = "<all old wasn't set>"
+        stop_last_str = '\t[stop]\tlast moment set to '
+        stop_old_str = '\t[stop]\told moments set to '
+        all_old_str = '\t[all]\told moments set to '
+        for l in self.lines:
+            pos = l.find(stop_last_str)
+            if pos != -1:
+                stop_last = l[pos + len(stop_last_str):-1]
+                break
+        for l in self.lines:
+            pos = l.find(stop_old_str)
+            if pos != -1:
+                stop_old = l[pos + len(stop_old_str):]
+        for l in self.lines:
+            pos = l.find(all_old_str)
+            if pos != -1:
+                all_old = l[pos + len(all_old_str):]
+        self.assertEqual(stop_old, all_old)
+        self.assertTrue(all_old.find(stop_last) != -1)
 
-    def test_input_set_in_right_order(self):
-        self.assertTrue(self.one_says("\tinput value set to 'hi'\n"))
-        self.assertTrue(self.one_says("\tinput value set to ' there'\n"))
-        self.assertTrue(self.one_says("\tinput value set to ''\n"))
+    def test_after_interrupt_last_moment_updates(self):
+        stop_last = "<it wasn't set actually>"
+        all_last = "<they weren't set actually>"
+        stop_last_set_to = '\t[stop]\tlast moment set to '
+        all_last_set_to = '\t[all]\tlast moment set to '
+        for l in self.lines:
+            pos = l.find(stop_last_set_to)
+            if pos != -1:
+                stop_last = l[pos + len(stop_last_set_to):]
+        for l in self.lines:
+            pos = l.find(all_last_set_to)
+            if pos != -1:
+                all_last = l[pos + len(all_last_set_to):]
+        self.assertEqual(stop_last, all_last)
 
-    def test_last_moment_set_right_number_of_times(self):
-        n = 0
-        for line in self.lines:
-            if line.find("\tlast moment set to [") != -1:
-                n += 1
-        self.assertEqual(3, n)
+    def test_and_the_messages_havent_changed(self):
+        m1 = '\t[stop]\tlast moment set to [{"name":"Vaggas1","message":[{"type":"write","body":"hi thereclear"}]}]'
+        m2 = '\t[stop]\tlast moment set to [{"name":"Sotiris2","message":[{"type":"write","body":"interrupt"}]}]' 
+        n1 = 0
+        n2 = 0
+        for l in self.lines:
+            if l.find(m1) != -1:
+                n1 += 1
+            if l.find(m2) != -1:
+                n2 += 1
+        self.assertEqual(1, n1)
+        self.assertEqual(1, n2)
 
-    def test_no_excess_events(self):
-        self.assertEqual(6, len(self.lines))
+    def test_unless_interrupt_old_moments_are_nothing(self):
+        say = '[say]\told moments set to [[]]'
+        hear = '[hear]\told moments set to [[]]'
+        stop = '[stop]\told moments set to [[]]'
+        n = {"say": 0, "hear": 0, "stop": 0}
+        for l in self.lines:
+            if l.find(say) != -1:
+                n["say"] += 1
+            if l.find(hear) != -1:
+                n["hear"] += 1
+            if l.find(stop) != -1:
+                n["stop"] += 1
+        self.assertEqual({"say": 1, "hear": 1, "stop": 1}, n)
+
+    def test_as_many_logs_as_last_test_run(self):
+        self.assertEqual(20, len(self.lines))
 
 
 if __name__ == "__main__":
