6f49d89c9d24e2c93aefd77fe1984253a8a9ab8e
Theodore Alenas <dimakopt732@gmail.com>
2024-08-08 10:35:32 +0300

    richer systest UI actions, no corresponding checks

--- js/mod/WebInteractor.systestMain.js
+++ js/mod/WebInteractor.systestMain.js
@@ -7,7 +7,7 @@ withRoom.catch(function(err) {
     console.error("Error, can't create room")
     throw err
 })
-const unreachable = withRoom.then(function() {
+const withNoError = withRoom.then(function() {
     fetch(roomUri, {method: "PUT"}).then(function(res) {
         if (res.status !== 409) {
             throw new Error("creating room twice, error code: " +
@@ -16,17 +16,17 @@ const unreachable = withRoom.then(function() {
     })
 })
 
-unreachable.then(shit)
-
-function shit() {
+function withWebInteractor(name, room, callback) {
     const wi = new WebInteractor(
         {
             webSocketUri: "ws://localhost:8001",
             lastMomentsUri: "http://localhost:8001/last"
         },
-        "my\nroom")
+        room)
 
-    function log(m) {console.log(Date.now() + "\t" + m)}
+    function log(m) {
+        console.log(Date.now() + "\t[" + name + "]\t" + m)
+    }
     wi.setSetInputValue(function(m) {
         log("input value set to '" + m + "'")
     })
@@ -38,9 +38,29 @@ function shit() {
     })
     const close = wi.getDestructor()
     wi.setOnReadySocket(function(unlocked) {
+        callback(unlocked, close)
+    })
+}
+
+withNoError.then(function() {
+    withWebInteractor("say", "my\nroom", function(unlocked, close) {
         unlocked.onInputChange("hi")
         unlocked.onInputChange("hi there")
         unlocked.onClear()
         setTimeout(close, 200)
     })
-}
+    withWebInteractor("hear", "my\nroom", function(unlocked, close) {
+        setTimeout(close, 200)
+    })
+    setTimeout(function() {
+        withWebInteractor("stop", "my\nroom", function(unlocked, close) {
+            unlocked.onInputChange("interrupt")
+            setTimeout(close, 200)
+        })
+    }, 600)
+    setTimeout(function() {
+        withWebInteractor("all", "my\nroom", function(unlocked, close) {
+            setTimeout(close, 200)
+        })
+    }, 800)
+})
--- scripts/test-sys.sh
+++ scripts/test-sys.sh
@@ -6,7 +6,7 @@ set -e
     cd ./python
     . venv/bin/activate
     export KEY_MOUTH_SHOW_WHO_TO=yes
-    timeout 2s uvicorn                          \
+    timeout 3s uvicorn                          \
             --host localhost                    \
             --port 8001                         \
             --log-config systest-log-config.ini \
