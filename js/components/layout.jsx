6f091917c254c91bbb0df342cf2eeba1acad9003
Theodore Alenas <dimakopt732@gmail.com>
2024-08-09 16:11:12 +0300

    centered the Home link in layout.jsx

--- js/components/layout.jsx
+++ js/components/layout.jsx
@@ -1,9 +1,13 @@
+import styles from './layout.module.css'
 import UriHome from '../mod/UriHome.js'
 
 export default function Layout({ env, children }) {
     const uri = new UriHome(env.home, 'ERROR, LAYOUT HAS NO ROOM')
     return <>
-               <a href={uri.home()}>Home</a>
+               <a href={uri.home()}
+                  className={styles.stickyTop + ' ' +
+                             styles.tmpCenter}
+               >Home</a>
                {children}
            </>
 }
