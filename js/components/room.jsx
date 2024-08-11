13286e09d1c28d5e017ae8f13530bf8bc8154e4b
Theodore Alenas <dimakopt732@gmail.com>
2024-08-11 17:19:07 +0300

    extracted room's main element into a component

---
+++ js/components/room.jsx
@@ -0,0 +1,15 @@
+import Moments from '../components/moments.jsx'
+import InputAndButton from '../components/inputAndButton.jsx'
+import styles from './room.module.css'
+
+export default function Room({o}) {
+    return (
+        <main className={styles.main + ' ' + styles.bgPale}>
+            <Moments o={o} />
+            <InputAndButton
+                o={o}
+                className={styles.bgPale + ' ' +
+                           styles.stickyBottom} />
+        </main>
+    )
+}
--- js/pages/room.jsx
+++ js/pages/room.jsx
@@ -1,14 +1,12 @@
 import WebInteractor from '../mod/WebInteractor.js'
 import UriRoom from '../mod/UriRoom.js'
 import uriFirstArg from '../mod/uriFirstArg.js'
-import styles from './room.module.css'
-import Moments from '../components/moments.jsx'
-import InputAndButton from '../components/inputAndButton.jsx'
+import Room from '../components/room.jsx'
 import Layout from '../components/layout.jsx'
 import { useEffect, useRef, useState } from "react"
 import { useRouter } from "next/router"
 
-export default function Home({env}) {
+export default function PageRoom({env}) {
     const [o, setO] = useState(null)
     const router = useRouter()
     useEffect(function() {
@@ -22,13 +20,7 @@ export default function Home({env}) {
         }
     }, [router.isReady])
     return <Layout env={env}>
-               <main className={styles.main + ' ' + styles.bgPale}>
-                   <Moments o={o} />
-                   <InputAndButton
-                       o={o}
-                       className={styles.bgPale + ' ' +
-                                  styles.stickyBottom} />
-               </main>
+               <Room o={o} />
            </Layout>
 }
 
