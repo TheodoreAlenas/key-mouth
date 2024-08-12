793db6f7166788dcd538bdedb849136df34952ac
Theodore Alenas <dimakopt732@gmail.com>
2024-08-12 22:04:39 +0300

    wired up my portfolio's theming code to the button

--- js/components/layout.jsx
+++ js/components/layout.jsx
@@ -2,6 +2,7 @@ import shapes from './shapes.module.css'
 import colors from './colors.module.css'
 import UriHome from '../mod/UriHome.js'
 import Link from 'next/link'
+import { useEffect, useState } from "react"
 
 export default function Layout({ env, showHome, children }) {
     const uri = new UriHome(env.home, 'ERROR, LAYOUT HAS NO ROOM')
@@ -24,7 +25,35 @@ function HomeLink({uri}) {
 }
 
 function ThemeToggle() {
+    useEffect(function() {
+        initColorScheme()
+    }, [])
     return <button className={shapes.button + ' ' +
                               colors.button}
+                   onClick={switchColorScheme}
            >Switch light/dark theme</button>
+}
+
+function initColorScheme() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const preferred = prefersDark ? "dark" : "light";
+    const lastPreferred = localStorage.getItem("lastPreferredColorScheme");
+    localStorage.setItem("lastPreferredColorScheme", preferred);
+
+    if (lastPreferred === preferred && localStorage.getItem("lastColorScheme"))
+        setColorScheme(localStorage.getItem("lastColorScheme"));
+    else
+        setColorScheme(preferred);
+}
+
+function switchColorScheme() {
+    if (localStorage.getItem("lastColorScheme") === "dark")
+        setColorScheme("light");
+    else
+        setColorScheme("dark");
+}
+function setColorScheme(value) {
+    document.querySelector("html").style.colorScheme = value;
+    document.querySelector("html").setAttribute("data-sch", value);
+    localStorage.setItem("lastColorScheme", value);
 }
