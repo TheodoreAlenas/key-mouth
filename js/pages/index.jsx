04defb2c766b59605ccd175bdc5ec8128e74ee21
Theodore Alenas <dimakopt732@gmail.com>
2024-08-14 11:07:20 +0300

    semi-extracted getRooms for index.jsx

--- js/pages/index.jsx
+++ js/pages/index.jsx
@@ -5,25 +5,30 @@ import Layout from '../components/layout.jsx'
 import { useEffect, useState } from "react"
 
 export default function Home({env}) {
-    const uri = new UriHome(env.home)
     const [rooms, setRooms] = useState(null)
+    const uri = new UriHome(env.home)
     useEffect(function() {
-        const with200 = fetch(uri.rooms()).then(function(res) {
-            if (res.status !== 200) {
-                setRooms(<code>ERROR</code>)
-                throw new Error("couldn't fetch " + uri.rooms())
-            }
-            return res.json()
-        })
-        with200.catch(function(err) {
-            console.error("can't JSON parse response of " + uri.rooms())
-            throw err
-        })
-        with200.then(function(json) {
-            setRooms(json.map(e => ({text: e, href: uri.room(e)})))
-        })
+        getRooms(uri).then(setRooms)
     }, [])
     return <Layout env={env}><RoomList rooms={rooms} /></Layout>
+}
+
+function getRooms(uri) {
+    const with200 = fetch(uri.rooms()).then(function(res) {
+        if (res.status !== 200) {
+            setRooms(<code>ERROR</code>)
+            throw new Error("couldn't fetch " + uri.rooms())
+        }
+        return res.json()
+    })
+    with200.catch(function(err) {
+        console.error("can't JSON parse response of " + uri.rooms())
+        throw err
+    })
+    const withJson = with200.then(
+        json => json.map(e => ({text: e, href: uri.room(e)})))
+
+    return withJson
 }
 
 export async function getStaticProps() {
