7f5ac022014c8fab6246717d84eadda3b101d395
Theodore Alenas <dimakopt732@gmail.com>
2024-08-15 19:12:09 +0300

    scripts/test-all.sh simplified

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -1,30 +1,23 @@
 #!/bin/sh
 
-keep_res() {
-    err=$?
-    if ! [ 0 = $err ]
-    then echo failed > git-ignores/test-result-"$1"
-    else echo passed > git-ignores/test-result-"$1"
-    fi
-}
-check() {
-    test passed = "$(head git-ignores/test-result-"$1")" || exit 1
-}
+f=git-ignores/test-result
+
 echo
-echo "        front end tests"
+echo " ===  front end tests  ==="
 echo
 node js/mod/presentMoment.test.js
-keep_res front
+echo $? > $f-front
 echo
-echo "        back end tests"
+echo " ===  back end tests  ==="
 echo
 python python/AfterSocketLogicTest.py
-keep_res back
+echo $? > $f-back
 echo
-echo "        system tests"
+echo " ===  system tests  ==="
 echo
 sh scripts/test-sys.sh
-keep_res sys
-check front
-check back
-check sys
+echo $? > $f-sys
+
+test 0 = "$(head $f-front)" || exit 1
+test 0 = "$(head $f-back)" || exit 1
+test 0 = "$(head $f-sys)" || exit 1
