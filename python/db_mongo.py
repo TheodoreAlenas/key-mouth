4c2d653fdb99304f13e65a073a54ad6ec62f1d06
Theodore Alenas <dimakopt732@gmail.com>
2024-08-16 19:09:36 +0300

    tried and not tested mongo implementation

--- python/db_mongo.py
+++ python/db_mongo.py
@@ -2,9 +2,97 @@
 # License at the bottom
 
 from db_exceptions import RoomExistsException, RoomDoesntExistException
-from db_mock import Db
+import server_only
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
 
-print("this is supposed to be the real database")
+
+class RoomMoments:
+
+    def __init__(self, r, db):
+        self._cached_last_moment = r['last']
+        self.last_moment_time = r['last']['time']
+        self.n = r['n']
+        self.name = r['_id']
+        self._db = db
+
+    def add_moment(self, time, moment):
+        mom = {"time": time, "moment": moment}
+        self._db['rooms'].update_one(
+            {"_id": self.name},
+            {"$push": {"moments": mom}}
+        )
+        self.n += 1
+        self.last_moment_time = time
+        self._cached_last_moment = mom
+
+    def get_last_few(self):
+        ms = self._db['rooms'].find_one(
+            {"_id": self.name},
+            {"moments": {"$slice": [-30, 30]}}
+        )
+        m = ms['moments']
+        return {
+            "start": self.n - len(m),
+            "end": self.n,
+            "moments": m
+        }
+
+    def get_len(self):
+        return self.n
+
+    def get_range(self, start, end):
+        if start == self.n - 1 and end == self.n:
+            return [self._cached_last_moment]
+        ms = self._db['rooms'].find_one(
+            {"_id": self.name},
+            {"moments": {"$slice": [start, end - start]}}
+        )
+        return ms['moments']
+
+
+class Db:
+
+    def __init__(self):
+        self._db = MongoClient(server_only.db_uri)['keymouth']
+        self._rooms = {}
+        projection = {
+            'n': {'$size': '$moments'},
+            'last': {'$arrayElemAt': ['$moments', -1]}
+        }
+        rs = self._db['rooms'].aggregate([{'$project': projection}])
+        for r in rs:
+            self._rooms[r['_id']] = RoomMoments(r=r, db=self._db)
+
+    def create_room(self, time, name):
+        try:
+            first_moment = {"time": time, "moment": []}
+            room = RoomMoments(
+                r={'_id': name, 'last': first_moment, 'n': 1},
+                db=self._db
+            )
+            self._db['rooms'].insert_one({
+                "_id": name,
+                "moments": [first_moment]
+            })
+            self._rooms[name] = room
+        except DuplicateKeyError:
+            self._rooms.pop(name)
+            raise RoomExistsException("[DbMoct] room '" + name +
+                                      "' already exists")
+
+    def get_room(self, name) -> RoomMoments:
+        if not name in self._rooms:
+            raise RoomDoesntExistException("[DbMock] room '" + name +
+                                           "' doesn't exist")
+        return self._rooms[name]
+
+    def get_rooms_and_their_last_moment_times(self):
+        res = []
+        for name in self._rooms:
+            res.append((name, self._rooms[name].last_moment_time))
+        return res
+
 
 """
 Copyright 2024 <dimakopt732@gmail.com>
