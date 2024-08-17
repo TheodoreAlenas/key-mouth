c0a108e2183a7da7509d511e3b324c572156d92e
Theodore Alenas <dimakopt732@gmail.com>
2024-08-18 02:23:15 +0300

    simplified uriFirstArg because it got skinny n old

--- js/mod/uriFirstArg.js
+++ js/mod/uriFirstArg.js
@@ -1,11 +1,8 @@
 
-function getArg() {
-    const ui = process.env.KEYMOUTH_UI
-    const api = process.env.KEYMOUTH_API
-    const ws = process.env.KEYMOUTH_WS
-    return {home: {ui, api}, room: {ws, api}}
-}
+const ui = process.env.KEYMOUTH_UI
+const api = process.env.KEYMOUTH_API
+const ws = process.env.KEYMOUTH_WS
 
-const uriFirstArg = getArg()
+const uriFirstArg = {home: {ui, api}, room: {ws, api}}
 
 export default uriFirstArg
