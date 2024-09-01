c543cee214e9b1fbb1c9f6ff9e11b6f0e8ad85ef
Theodore Alenas <dimakopt732@gmail.com>
2024-09-01 13:35:54 +0300

    extracted moment.jsx

---
+++ js/components/moment.jsx
@@ -0,0 +1,61 @@
+
+export default function Moment({moment, styles}) {
+    try {
+        const m = moment
+        const namesAndCommas = m.names.reduce((a, x) => a + ', ' + x, '')
+        const msgs = m.messages.map(
+            (person, i) => PersonToLi({person, i, styles}))
+        return (
+            <section key={m.key} className={styles.moment}>
+                <h2 className={styles.bubbleTop}>
+                    {(m.time || '') + namesAndCommas}
+                </h2>
+                <ul className={styles.bubbleGroup + ' ' +
+                               styles.bubbleGroupSpacing + ' ' +
+                               styles.noBullets}>
+                    {msgs}
+                </ul>
+            </section>
+        )
+    }
+    catch (e) {
+        console.error("error rendering moment:")
+        console.error(moment)
+        throw e
+    }
+}
+
+function PersonToLi({person, i, styles}) {
+    try {
+        return <li id={person.id} key={i}>
+                   <pre className={styles.bubble}>
+                       {person.message.map(
+                           (diff, i) => DiffToSpan({diff, i, styles}))}
+                   </pre>
+               </li>
+    }
+    catch (e) {
+        console.error("error rendering person " + i + ':')
+        console.error(person)
+        throw e
+    }
+ }
+
+function DiffToSpan({diff, i, styles}) {
+    if (diff.type === "write") {
+        return <span key={i}>{diff.body}</span>
+    }
+    if (diff.type === "delete") {
+        return <del className={styles.delete}
+                    key={i}>{diff.body}</del>
+    }
+    if (diff.type === "erase") {
+        return <del className={styles.erase}
+                    key={i}>{diff.body}</del>
+    }
+    if (diff.type === "event") {
+        return <code className={styles.event}
+                     key={i}>{diff.body}</code>
+    }
+    else return <code key={i}>ERROR</code>
+}
--- js/components/moments.jsx
+++ js/components/moments.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react"
 import { getIsAtBottom, scrollToBottom } from "./scrolling.js"
+import Moment from './moment.jsx'
 
 export default function Moments({o, styles}) {
     const [state, setState] = useState({atBottom: true, moments: []})
@@ -19,7 +20,7 @@ export default function Moments({o, styles}) {
     let finalPres = <code>{"ERROR"}</code>
     try {
         const pres = state.moments.map(
-            moment => MomentToBubble({moment, styles}))
+            moment => Moment({moment, styles}))
         finalPres = pres
     }
     catch (err) {
@@ -31,65 +32,4 @@ export default function Moments({o, styles}) {
                     ref={ref}
                     className={styles.chat}
            >{finalPres}</section>
-}
-
-function MomentToBubble({moment, styles}) {
-    try {
-        const m = moment
-        const namesAndCommas = m.names.reduce((a, x) => a + ', ' + x, '')
-        const msgs = m.messages.map(
-            (person, i) => PersonToLi({person, i, styles}))
-        return (
-            <section key={m.key} className={styles.moment}>
-                <h2 className={styles.bubbleTop}>
-                    {(m.time || '') + namesAndCommas}
-                </h2>
-                <ul className={styles.bubbleGroup + ' ' +
-                               styles.bubbleGroupSpacing + ' ' +
-                               styles.noBullets}>
-                    {msgs}
-                </ul>
-            </section>
-        )
-    }
-    catch (e) {
-        console.error("error rendering moment:")
-        console.error(moment)
-        throw e
-    }
-}
-
-function PersonToLi({person, i, styles}) {
-    try {
-        return <li id={person.id} key={i}>
-                   <pre className={styles.bubble}>
-                       {person.message.map(
-                           (diff, i) => DiffToSpan({diff, i, styles}))}
-                   </pre>
-               </li>
-    }
-    catch (e) {
-        console.error("error rendering person " + i + ':')
-        console.error(person)
-        throw e
-    }
- }
-
-function DiffToSpan({diff, i, styles}) {
-    if (diff.type === "write") {
-        return <span key={i}>{diff.body}</span>
-    }
-    if (diff.type === "delete") {
-        return <del className={styles.delete}
-                    key={i}>{diff.body}</del>
-    }
-    if (diff.type === "erase") {
-        return <del className={styles.erase}
-                    key={i}>{diff.body}</del>
-    }
-    if (diff.type === "event") {
-        return <code className={styles.event}
-                     key={i}>{diff.body}</code>
-    }
-    else return <code key={i}>ERROR</code>
 }
