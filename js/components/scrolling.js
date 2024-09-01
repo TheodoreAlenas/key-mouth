bd03de7d830d38c9a3eb5e4be466de15e15a88b3
Theodore Alenas <dimakopt732@gmail.com>
2024-09-01 14:24:36 +0300

    sticky on mobile, grid on desktop

--- js/components/scrolling.js
+++ js/components/scrolling.js
@@ -1,5 +1,19 @@
 
 export function getIsAtBottom(element) {
+    if (window.innerWidth <= 400) {
+        return getIsAtBottom_window()
+    }
+    return getIsAtBottom_element(element)
+}
+
+function getIsAtBottom_window() {
+    const visible = window.innerHeight
+    const above = Math.floor(window.scrollY)
+    const all = document.documentElement.scrollHeight
+    return visible + above + 5 > all
+}
+
+function getIsAtBottom_element(element) {
     const visible = element.clientHeight
     const above = Math.floor(element.scrollTop)
     const all = element.scrollHeight
@@ -7,6 +21,17 @@ export function getIsAtBottom(element) {
 }
 
 export function scrollToBottom(element) {
-    element.scrollTop = element.scrollHeight
+    if (window.innerWidth <= 400) {
+        scrollToBottom_window()
+    }
+    else {
+        scrollToBottom_element(element)
+    }
 }
 
+function scrollToBottom_window() {
+    window.scrollTo({top: document.documentElement.scrollHeight})
+}
+function scrollToBottom_element(element) {
+    element.scrollTop = element.scrollHeight
+}
